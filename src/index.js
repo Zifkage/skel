88b8fa15dc68145b183ac567eb3d47be83dd4f05
Zifkage <zifkage@gmail.com>
2018-11-02 18:13:17 +0100

    Handle create user calls with empty payload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import '@babel/polyfill';
 import http from 'http';
 
 const requestHandler = function handler(req, res) {
+  if (req.method === 'POST' && req.url === '/users') {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Payload should not be empty' }));
+    return;
+  }
+
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Nazif');
 };
