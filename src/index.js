93041d5619df6dddccf89c26de13a4e72fb6678a
Zifkage <zifkage@gmail.com>
2018-11-04 18:24:25 +0100

    Check data type of Create User endpoint payload

--- src/index.js
+++ src/index.js
@@ -83,6 +83,18 @@ app.post('/users', (req, res) => {
     res.json({
       message: 'Payload must contain at least the email and password fields',
     });
+    return;
+  }
+  if (
+    typeof req.body.email !== 'string' ||
+    typeof req.body.password !== 'string'
+  ) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'The email and password fields must be of type string',
+    });
+    return;
   }
 });
 
