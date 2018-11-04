f57a997a34621ca0219d2ba9900da4930806402e
Zifkage <zifkage@gmail.com>
2018-11-04 01:00:35 +0100

    Migrate API to Express

--- src/index.js
+++ src/index.js
@@ -1,52 +1,49 @@
 import '@babel/polyfill';
-import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-const requestHandler = function handler(req, res) {
-  if (req.method === 'POST' && req.url === '/users') {
-    const payloadData = [];
-    req.on('data', (data) => {
-      payloadData.push(data);
+const app = express();
+
+app.use(bodyParser.json({ limit: 1e6 }));
+
+app.post('/users', (req, res) => {
+  if (parseInt(req.headers['content-length'], 10) === 0) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'Payload should not be empty',
     });
-
-    req.on('end', () => {
-      if (payloadData.length === 0) {
-        res.writeHead(400, { 'Content-Type': 'application/json' });
-        res.end(
-          JSON.stringify({
-            message: 'Payload should not be empty',
-          }),
-        );
-        return;
-      }
-
-      if (req.headers['content-type'] !== 'application/json') {
-        res.writeHead(415, { 'Content-Type': 'application/json' });
-        res.end(
-          JSON.stringify({
-            message:
-              'The "Content-Type" header must always be "application/json"',
-          }),
-        );
-        return;
-      }
-
-      try {
-        const bodyString = Buffer.concat(payloadData).toString();
-        JSON.parse(bodyString);
-      } catch (e) {
-        res.writeHead(400, { 'Content-type': 'application/json' });
-        res.end(
-          JSON.stringify({
-            message: 'Payload should be in JSON format',
-          }),
-        );
-      }
-    });
-  } else {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Nazif');
+    return;
   }
-};
 
-const server = http.createServer(requestHandler);
-server.listen(8080);
+  if (req.headers['content-type'] !== 'application/json') {
+    res.status(415);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'The "Content-Type" header must always be "application/json"',
+    });
+  }
+});
+
+/* eslint-disable */
+app.use(function(err, req, res, next) {
+  if (
+    err instanceof SyntaxError &&
+    err.status === 400 &&
+    'body' in err &&
+    err.type === 'entity.parse.failed'
+  ) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'Payload should be in JSON format',
+    });
+  }
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `Hobnod API server listening on port ${process.env.SERVER_PORT}!`,
+  );
+});
