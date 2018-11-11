2affd55e89192f8284ba98eaeb4cf40f38c9c00c
Zifkage <zifkage@gmail.com>
2018-11-11 18:25:54 +0100

    Add unit test for Create User request  handler

--- src/handlers/users/create/index.unit.test.js
+++ src/handlers/users/create/index.unit.test.js
@@ -1,8 +1,162 @@
-import { stub } from 'sinon';
+import { stub, spy } from 'sinon';
 import ValidationError from '../../../validators/errors/validation-error';
+import createUser from '.';
+import assert from 'assert';
 
 const createStubs = {
-  success: stub().resolves({ _id: 'foo' }),
-  validationError: stub().rejects(new ValidationError()),
-  otherError: stub().rejects(new Error())
+  success: () => stub().resolves({ _id: 'foo' }),
+  validationError: () => stub().rejects(new ValidationError('error message')),
+  otherError: () => stub().rejects(new Error())
 };
+
+describe('Create User request handler', function() {
+  let req;
+  let res;
+  let db;
+
+  describe("When create resolves with the new user's ID", function() {
+    beforeEach(function() {
+      db = {};
+      req = {};
+      res = {
+        status: spy(),
+        set: spy(),
+        send: spy()
+      };
+
+      return createUser(req, res, db, createStubs.success(), ValidationError);
+    });
+
+    describe('should call res.status() once', function() {
+      it('once', function() {
+        assert(res.status.calledOnce);
+      });
+
+      it('with the arguments 200', function() {
+        assert(res.status.calledWithExactly(201));
+      });
+    });
+
+    describe('should call res.set()', function() {
+      it('once', function() {
+        assert(res.set.calledOnce);
+      });
+
+      it('with the arguments "Content-tyoe" and "text/plain"', function() {
+        assert(res.set.calledWithExactly('Content-Type', 'text/plain'));
+      });
+    });
+
+    describe('should call res.send()', function() {
+      it('once', function() {
+        assert(res.send.calledOnce);
+      });
+
+      it('with the user id as paramater', function() {
+        assert(res.send.calledWithExactly('foo'));
+      });
+    });
+  });
+
+  describe('When create reject with ValidationError instance', function() {
+    beforeEach(function() {
+      db = {};
+      req = {};
+      res = {
+        status: spy(),
+        set: spy(),
+        json: spy()
+      };
+
+      return createUser(
+        req,
+        res,
+        db,
+        createStubs.validationError(),
+        ValidationError
+      );
+    });
+
+    describe('should call res.status() once', function() {
+      it('once', function() {
+        assert(res.status.calledOnce);
+      });
+
+      it('with the arguments 400', function() {
+        assert(res.status.calledWithExactly(400));
+      });
+    });
+
+    describe('should call res.set()', function() {
+      it('once', function() {
+        assert(res.set.calledOnce);
+      });
+
+      it('with the arguments "Content-tyoe" and "application/json"', function() {
+        assert(res.set.calledWithExactly('Content-Type', 'application/json'));
+      });
+    });
+
+    describe('should call res.json()', function() {
+      it('once', function() {
+        assert(res.json.calledOnce);
+      });
+
+      it('with the correct error object', function() {
+        assert(res.json.calledWithExactly({ message: 'error message' }));
+      });
+    });
+  });
+
+  describe('When create catch other error', function() {
+    beforeEach(function() {
+      db = {};
+      req = {};
+      res = {
+        status: spy(),
+        set: spy(),
+        json: spy()
+      };
+
+      return createUser(
+        req,
+        res,
+        db,
+        createStubs.otherError(),
+        ValidationError
+      );
+    });
+
+    describe('should call res.status() once', function() {
+      it('once', function() {
+        assert(res.status.calledOnce);
+      });
+
+      it('with the arguments 500', function() {
+        assert(res.status.calledWithExactly(500));
+      });
+    });
+
+    describe('should call res.set()', function() {
+      it('once', function() {
+        assert(res.set.calledOnce);
+      });
+
+      it('with the arguments "Content-tyoe" and "application/json"', function() {
+        assert(res.set.calledWithExactly('Content-Type', 'application/json'));
+      });
+    });
+
+    describe('should call res.json()', function() {
+      it('once', function() {
+        assert(res.json.calledOnce);
+      });
+
+      it('with the correct error object', function() {
+        assert(
+          res.json.calledWithExactly({ message: 'Internal Server Error' })
+        );
+      });
+    });
+  });
+});
